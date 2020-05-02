abd6ad1065c9791d2f96b0f2a4195531c9411e88
Eliav <eliav.s.cohen@gmail.com>
2020-05-02 16:28:08 +0300

    backend tested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { GoogleAnalyticsEventsService } from './services/GoogleAnalyticsEvents/GoogleAnalyticsEvents.service';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './components/example/example.component';
+
+// services
+import { GoogleAnalyticsEventsService } from './services/GoogleAnalyticsEvents/GoogleAnalyticsEvents.service';
+import { ExampleService } from './services/example/example.service';
 
 @NgModule({
 	declarations: [
@@ -13,11 +17,13 @@ import { ExampleComponent } from './components/example/example.component';
 	],
 	imports: [
 		BrowserModule,
+		HttpClientModule,
 		AppRoutingModule
 	],
 	providers: [
 		GoogleAnalyticsEventsService,
-		Title
+		Title,
+		ExampleService
 	],
 	bootstrap: [
 		AppComponent
--- src/app/components/example/example.component.ts
+++ src/app/components/example/example.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 import { GoogleAnalyticsEventsService } from './../../services/GoogleAnalyticsEvents/GoogleAnalyticsEvents.service';
+import { ExampleService } from './../../services/example/example.service';
 
 @Component({
 	selector: 'bk-example',
@@ -10,10 +12,19 @@ import { GoogleAnalyticsEventsService } from './../../services/GoogleAnalyticsEv
 })
 export class ExampleComponent implements OnInit {
 
-	constructor(private googleAnalyticsEventsService: GoogleAnalyticsEventsService, private titleService: Title) { }
+	info;
+
+	constructor(
+		private googleAnalyticsEventsService: GoogleAnalyticsEventsService,
+		private titleService: Title,
+		private exampleService: ExampleService) { }
 
 	ngOnInit() {
 		this.titleService.setTitle(this.titleService.getTitle() + ' | Exmaple');
+		this.exampleService.getInfo().subscribe(info => {
+			this.info = info;
+			console.log(info);
+		});
 	}
 
 	addEvent(eventName: string, eventCategory: string, eventAction: string, eventLabel?: string, eventValue?: number) {
