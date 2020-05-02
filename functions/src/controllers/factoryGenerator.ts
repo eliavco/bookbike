916f3234e6c363fdb8a3f8b03295631657004785
Eliav <eliav.s.cohen@gmail.com>
2020-05-02 22:07:04 +0300

    db2

--- functions/src/controllers/factoryGenerator.ts
+++ functions/src/controllers/factoryGenerator.ts
@@ -2,9 +2,15 @@ const catchAsync = require('./../utils/catchAsync');
 
 exports.getAll = (resource: string) => {
 	return catchAsync((async (req: any, res: any) => {
-		res.status(200).json({
-			status: 'success',
-			resource
-		});
+		try {
+			const documents = await req.db.collection(resource).get();
+			res.status(200).json({
+				status: 'success',
+				resource,
+				documents
+			});
+		} catch (err) {
+			res.status(404).json({ status: 'failure' });
+		}
 	}));
 };
