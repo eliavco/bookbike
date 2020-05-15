90731a339a903a598049ebfeb0be3a8a1b7c512c
Eliav <eliav.s.cohen@gmail.com>
2020-05-15 19:21:36 +0300

    404, local https and icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ExampleComponent } from './components/example/example.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
 	{ path: 'app', component: ExampleComponent },
 	{ path: '', component: ExampleComponent },
+	{ path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
