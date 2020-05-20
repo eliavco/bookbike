e9c2c4af057b10a8b562ad70f5a9bb5d219492fb
Eliav <eliav.s.cohen@gmail.com>
2020-05-20 21:02:55 +0300

    Desktop packaging

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -7,8 +7,15 @@ import { Title } from '@angular/platform-browser';
 	styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
+	private base = '/';
+	styleBgPurple;
+	styleBgStars;
 
-	constructor(private titleService: Title) { }
+	constructor(private titleService: Title) {
+		if (navigator.userAgent.toLowerCase().indexOf(' electron/') > -1) { this.base = './'; }
+		this.styleBgPurple = `background: url("${this.base}assets/404/bg_purple.png");`;
+		this.styleBgStars = `background: url("${this.base}assets/404/overlay_stars.svg");`;
+	}
 
 	ngOnInit(): void {
 		this.titleService.setTitle(`${(window as any).bkBaseTitle} | Not Found`);
